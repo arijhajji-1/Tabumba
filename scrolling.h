161e28c39520c359e227385a824780a1317eeb9a
arijhajji99 <61475226+arijhajji99@users.noreply.github.com>
2020-03-06 14:36:48 +0100

    Add files via upload

---
+++ scrolling.h
@@ -0,0 +1,3 @@
+void initialiser (SDL_Rect *positionFond,SDL_Rect *positionper,SDL_Surface *imageDeFond ,SDL_Surface *pers );
+void afficher (SDL_Rect *positionFond,SDL_Rect *positionper,SDL_Surface *imageDeFond ,SDL_Surface *pers,SDL_Surface *ecran) ;
+void scrolling_map ();
