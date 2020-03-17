1f81b214b68894915d1c55a99bd44a2086b5e659
arijhajji99 <61475226+arijhajji99@users.noreply.github.com>
2020-03-17 10:56:32 +0100

    Update collisionbg.h

--- collisionbg.h
+++ collisionbg.h
@@ -1,4 +1,4 @@
-SDL_Color GetPixel(SDL_Surface *pSurface,int x,int y);
+
 void update_collision(personnage* perso);
-void dep(SDL_Rect *pos,SDL_Surface *perso,SDL_Surface *ecran);
+
 int collision_player_map(SDL_Surface *ecran , personnage* perso);
