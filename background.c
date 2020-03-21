619cde6988232061d658d475d4c5b17defa65375
arijhajji99 <61475226+arijhajji99@users.noreply.github.com>
2020-03-21 15:03:47 +0100

    Update background.c

--- background.c
+++ background.c
@@ -6,28 +6,116 @@
 #include "SDL/SDL_ttf.h"
 #include "background.h"
 
-SDL_Surface * Init_imagedefond( SDL_Surface *imagedefond ,SDL_Rect *image ,  int x , int y) 
+SDL_Surface * Init_imagedefond( SDL_Surface *imagedefond ,SDL_Rect *positionFond ,  int x , int y) 
  {
 	       imagedefond =  IMG_Load("map.png"); 
 
 	      if  ( imagedefond ==  NULL )  { 
-         printf ( "Can not load image : %s \n " , SDL_GetError ()); 
-         
+         printf ( "Can not load image of tux: %s \n " , SDL_GetError ()); 
+         exit ( 1 ); 
      } 
      else 
      {
  
-     image->x  =  x ; 
-     image->y  = y ;
-     image->h= imagedefond->h ;
-     image->w= imagedefond->w ;  
+     positionFond->x  =  x ; 
+     positionFond->y  = y ;
+     positionFond->h= imagedefond->h ;
+     positionFond->w= imagedefond->w ;  
  }
      
      return (imagedefond) ; 
 }
- void Show_imagedefond (SDL_Surface *imagedefond , SDL_Surface *screen , SDL_Rect image ) 
+ void Show_imagedefond (SDL_Surface *imagedefond , SDL_Surface *screen , SDL_Rect positionFond ) 
   {
 
-	 SDL_BlitSurface(imagedefond, NULL, screen, &image);
+	 SDL_BlitSurface(imagedefond, NULL, screen, &positionFond);
 }
+void scrolling_map (SDL_Rect *positionFond,SDL_Rect *positionper,SDL_Surface *imageDeFond ,SDL_Surface *pers,SDL_Surface *screen)
 
+
+{
+
+SDL_Event event;
+    SDL_EnableKeyRepeat(10, 10);
+    int continuer = 1;
+
+while (continuer)
+{
+
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                continuer = 0;
+                break;
+            case SDL_KEYDOWN:
+            switch(event.key.keysym.sym)
+
+        {
+
+            case SDLK_RIGHT: // Flèche droite
+            	
+                
+        
+                       positionFond->x ++;
+                       SDL_BlitSurface(imageDeFond,positionFond, screen, NULL);
+                   
+                 
+            
+                
+                SDL_Flip(screen);
+
+                break;
+
+            case SDLK_LEFT: // Flèche gauche
+            	
+        
+            positionFond->x --;
+        
+    
+         
+                SDL_BlitSurface(imageDeFond,positionFond, screen, NULL);
+				    
+                
+               SDL_Flip(screen);
+
+                break;
+
+            case SDLK_UP: 
+            	
+        
+                       positionFond->y --;
+                       SDL_BlitSurface(imageDeFond,positionFond, screen, NULL);
+                   
+                 
+            
+                
+                SDL_Flip(screen);
+
+                break;
+               case SDLK_DOWN: 
+            	
+        
+                       positionFond->y ++ ;
+                       SDL_BlitSurface(imageDeFond,positionFond, screen, NULL);
+                   
+                 
+            
+                
+                SDL_Flip(screen);
+
+                break;
+
+        }
+
+        break;    
+        }
+
+
+                SDL_Flip(screen);
+ }               SDL_Delay(10);
+
+
+
+
+}
