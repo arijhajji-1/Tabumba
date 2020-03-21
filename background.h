37f23186d5d8aef3964d7b0c9e0d9c83b38a4ca5
arijhajji99 <61475226+arijhajji99@users.noreply.github.com>
2020-03-21 15:05:15 +0100

    Update background.h

--- background.h
+++ background.h
@@ -1,2 +1,3 @@
-SDL_Surface* Init_imagedefond( SDL_Surface *imagedefond ,SDL_Rect *image ,  int x , int y) ;
-void Show_imagedefond (SDL_Surface *imagedefond , SDL_Surface *screen ,  SDL_Rect image  ) ;
+SDL_Surface* Init_imagedefond( SDL_Surface *imagedefond ,SDL_Rect *positionFond ,  int x , int y) ;
+void Show_imagedefond (SDL_Surface *imagedefond , SDL_Surface *screen ,  SDL_Rect positionFond  ) ;
+void scrolling_map (SDL_Rect *positionFond,SDL_Rect *positionper,SDL_Surface *imageDeFond ,SDL_Surface *pers,SDL_Surface *screen);
