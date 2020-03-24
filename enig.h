bf44b01a3d37f2b356a4add4999e941cb34ecb01
yahya982 <61556856+yahya982@users.noreply.github.com>
2020-03-24 13:48:35 -0700

    Add files via upload

---
+++ enig.h
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_mixer.h>
+#include <SDL/SDL_image.h> 
+#include "SDL/SDL_ttf.h"
+
+typedef struct 
+{
+ SDL_Surface *img;
+ SDL_Rect 	p; 
+ 
+}enigme;
+
+void init_enigme(enigme * e); 
+void  generate_afficher (SDL_Surface * screen  , char image [],enigme *e,int *alea) ;
+int solution_e (char image []) ;
+int resolution (int * running,int *run);
+void afficher_resultat (SDL_Surface * screen,int s,int r,enigme *en) ;
