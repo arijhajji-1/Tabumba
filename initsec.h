6536f85b7a5b11152ea5171aa62bdf21cced85c4
arijhajji99 <61475226+arijhajji99@users.noreply.github.com>
2020-04-10 19:51:03 +0100

    Update initsec.h

--- initsec.h
+++ initsec.h
@@ -1,13 +1,17 @@
-typedef struct 
+typedef struct Enemy
 {
-SDL_Surface *image ; 
-SDL_Rect positionAnimation ; 
-SDL_Rect positionAbsolute;
-int direction;
-int moving;
-int health;
-} character ; 
-void inisialiser_character (character *t, int x ,int y);
-void afficher_character (character *t,SDL_Surface *screen,SDL_Rect pos);
-void animation (character *t);
+  SDL_Surface *image[3];
+  SDL_Surface *image2[3];
+  SDL_Surface *Hit;
+  SDL_Surface *imageActuel;
+  SDL_Rect position;
+  SDL_Rect positionO;
+  int frame;
+  int direction;
+  int died;
+
+}Enemy; 
+void inisialiser_character (Enemy *t, int x ,int y);
+void afficher_character (Enemy *t,SDL_Surface *screen,SDL_Rect pos);
+void animation (Enemy *t);
 
