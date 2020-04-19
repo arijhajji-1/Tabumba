50aca74439bb971638e7f65336f425d04b181995
arijhajji99 <61475226+arijhajji99@users.noreply.github.com>
2020-04-19 22:15:11 +0100

    Update background.c

--- background.c
+++ background.c
@@ -1,121 +1,50 @@
-#include <stdio.h>
-#include  <stdlib.h>
-#include  <SDL/SDL.h>
-#include <SDL/SDL_image.h>
-#include <SDL/SDL_mixer.h>
-#include "SDL/SDL_ttf.h"
 #include "background.h"
 
-SDL_Surface * Init_imagedefond( SDL_Surface *imagedefond ,SDL_Rect *positionFond ,  int x , int y) 
- {
-	       imagedefond =  IMG_Load("map.png"); 
-
-	      if  ( imagedefond ==  NULL )  { 
-         printf ( "Can not load image of tux: %s \n " , SDL_GetError ()); 
-         exit ( 1 ); 
-     } 
-     else 
-     {
- 
-     positionFond->x  =  x ; 
-     positionFond->y  = y ;
-     positionFond->h= imagedefond->h ;
-     positionFond->w= imagedefond->w ;  
- }
-     
-     return (imagedefond) ; 
-}
- void Show_imagedefond (SDL_Surface *imagedefond , SDL_Surface *screen , SDL_Rect positionFond ) 
-  {
-
-	 SDL_BlitSurface(imagedefond, NULL, screen, &positionFond);
-}
-void scrolling_map (SDL_Rect *positionFond,SDL_Rect *positionper,SDL_Surface *imageDeFond ,SDL_Surface *pers,SDL_Surface *screen)
 
 
+void initialiser_backround (background *b)
 {
+b->x_relative=0;
+b->y_relative=0;
+b->background=NULL;
+b->bg=NULL;
+b->pos.x=0;
+b->pos.y=0;
+b->camera.x=0;
+b->camera.y=0;
+b->camera.w=10000;
+b->camera.h=800;
+b->background=IMG_Load("map.png");
+b->bg=IMG_Load("masks.png");
 
-SDL_Event event;
-    SDL_EnableKeyRepeat(10, 10);
-    int continuer = 1;
+}
 
-while (continuer)
+void afficher_background (SDL_Surface *screen,background *b)
 {
-
-        SDL_WaitEvent(&event);
-        switch(event.type)
-        {
-            case SDL_QUIT:
-                continuer = 0;
-                break;
-            case SDL_KEYDOWN:
-            switch(event.key.keysym.sym)
-
-        {
-
-            case SDLK_RIGHT: // Flèche droite
-            	
-                
-        
-                       positionFond->x ++;
-                       SDL_BlitSurface(imageDeFond,positionFond, screen, NULL);
-                   
-                 
-            
-                
-                SDL_Flip(screen);
-
-                break;
-
-            case SDLK_LEFT: // Flèche gauche
-            	
-        
-            positionFond->x --;
-        
-    
-         
-                SDL_BlitSurface(imageDeFond,positionFond, screen, NULL);
-				    
-                
-               SDL_Flip(screen);
-
-                break;
-
-            case SDLK_UP: 
-            	
-        
-                       positionFond->y --;
-                       SDL_BlitSurface(imageDeFond,positionFond, screen, NULL);
-                   
-                 
-            
-                
-                SDL_Flip(screen);
-
-                break;
-               case SDLK_DOWN: 
-            	
-        
-                       positionFond->y ++ ;
-                       SDL_BlitSurface(imageDeFond,positionFond, screen, NULL);
-                   
-                 
-            
-                
-                SDL_Flip(screen);
-
-                break;
-
-        }
-
-        break;    
-        }
+  SDL_BlitSurface(b->bg,NULL,screen,&b->pos);
+SDL_BlitSurface(b->background,&b->camera,screen,NULL);
 
 
-                SDL_Flip(screen);
- }               SDL_Delay(10);
+}
 
+void scrolling_right (background *b,personnageP *p,SDL_Surface *screen)
+{
+	if (b->camera.x<10000-1010)
+	{
+		b->x_relative+=p->xvelocity;
+		b->camera.x+=p->xvelocity;
+    p->position.x+=p->xvelocity;
 
+	}
+}
 
+void scrolling_left (background *b,personngeP *p,SDL_Surface *screen)
+{
+	if (b->camera.x>0)
+	{
+		b->x_relative-=p->xvelocity;
+		b->camera.x-=p->xvelocity;
+    p->position.x-=p->xvelocity;
 
+	}
 }
