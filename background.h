0254771708d2e9a74d76eeb74b46bf36dcb8fe59
arijhajji99 <61475226+arijhajji99@users.noreply.github.com>
2020-04-19 19:19:28 +0100

    Update background.h

--- background.h
+++ background.h
@@ -1,3 +1,6 @@
+#include "pp.h"
+
+
 SDL_Surface* Init_imagedefond( SDL_Surface *imagedefond ,SDL_Rect *positionFond ,  int x , int y) ;
 void Show_imagedefond (SDL_Surface *imagedefond , SDL_Surface *screen ,  SDL_Rect positionFond  ) ;
 void scrolling_map (SDL_Rect *positionFond,SDL_Rect *positionper,SDL_Surface *imageDeFond ,SDL_Surface *pers,SDL_Surface *screen);
